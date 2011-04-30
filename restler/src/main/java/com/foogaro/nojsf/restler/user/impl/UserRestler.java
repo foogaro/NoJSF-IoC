8e1b4b51c2bebd8d28889fcf11237035f9781abf
Luigi Fugaro <l.fugaro@gmail.com>
2011-04-30 23:06:48 +0200

    Fix scan jax-rs resources

--- restler/src/main/java/com/foogaro/nojsf/restler/user/impl/UserRestler.java
+++ restler/src/main/java/com/foogaro/nojsf/restler/user/impl/UserRestler.java
@@ -2,12 +2,13 @@ package com.foogaro.nojsf.restler.user.impl;
 
 import com.foogaro.nojsf.business.exception.BusinessException;
 import com.foogaro.nojsf.business.user.IUserBusiness;
-import com.foogaro.nojsf.model.DataWrapper;
 import com.foogaro.nojsf.model.ResponseWrapper;
 import com.foogaro.nojsf.model.UserDTO;
 import com.foogaro.nojsf.restler.ACRUDRestler;
 import com.foogaro.nojsf.restler.exception.RestlerException;
 import com.foogaro.nojsf.restler.user.IUserRestler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,12 +16,15 @@ import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
+import javax.ws.rs.ext.Provider;
 import java.util.List;
 
+@Provider
 @Path("/users")
 public class UserRestler extends ACRUDRestler<UserDTO> implements IUserRestler {
 
+    private Logger logger = LoggerFactory.getLogger(UserRestler.class);
+    
     @Autowired
     private IUserBusiness userBusiness;
 
@@ -30,12 +34,14 @@ public class UserRestler extends ACRUDRestler<UserDTO> implements IUserRestler {
         List<UserDTO> userDTOList;
         try {
             userDTOList = userBusiness.findAll();
+            logger.debug("userDTOList: " + userDTOList);
             ResponseWrapper responseWrapper = new ResponseWrapper();
             responseWrapper.setDataWrapper(userDTOList);
             res = Response.ok(responseWrapper).build();
         } catch (BusinessException e) {
             e.printStackTrace();
             res = Response.serverError().build();
+            logger.debug("BusinessException: " + e);
         }
 
 //        ResponseWrapper responseWrapper = new ResponseWrapper();
